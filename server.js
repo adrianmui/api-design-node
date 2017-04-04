6b3df8d6da2293881b646c43386adce66b520c5f
Adrian Mui <adrian.mui@gmail.com>
2017-04-04 02:38:00 -0500

    tried to mess around with templating language but realize u need somethined like jade

--- server.js
+++ server.js
@@ -2,4 +2,58 @@
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
 
-var jsonData = {count: 12, message: 'hey'};
+// fake data
+let jsonData = {
+    count: 12,
+    message: 'hey'
+};
+
+//express server
+const express = require('express');
+const fs = require('fs');
+let app = express();
+
+app.set('view engine', 'ejs');
+
+//middleware with IIFE
+app.use(
+    ((data) => {
+        return (request, response, callback) => {
+            console.log('response has set data to: ', data);
+            request.body = {};
+            request.body.count = data.count;
+            request.body.message = data.message;
+            callback();
+        }
+    })(jsonData)
+);
+
+//entry point
+app.get('/', (request, response) => {
+    // synchronously processes the view language
+    // response.sendFile(__dirname + '/index.html', () => {
+
+    // });
+    // response.send(` 
+    //   ${request.user.name} + ${request.body.count} + ${request.body.message}
+    // `);
+    fs.readFile('index.html', (err, buffer) => {
+        if (err) console.log(err);
+        let html = buffer.toString();
+
+        response.send(`
+        {
+           count: ${request.body.count},
+           message: ${request.body.message}
+        } + ${html}`);
+    });
+});
+
+app.get('/data', (request, response) => {
+    response.send(jsonData);
+})
+
+// port listen
+app.listen('3000', () => {
+    console.log('listening to port 3000...')
+});
