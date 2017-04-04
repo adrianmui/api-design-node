ec982410284edb46fa1d098504b2cdbab0efe911
Adrian Mui <adrian.mui@gmail.com>
2017-04-04 11:38:14 -0500

    finished lions crud api, validations should be done on the db level

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 // TODO: make this work.
 // if yuo go to localhost:3000 the app
 // there is expected crud to be working here
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const _ = require('underscore');
 
 // express.static will serve everything
 // with in client as a static resource
@@ -14,14 +14,72 @@ app.use(express.static('client'));
 
 // body parser makes it possible to post JSON to the server
 // we can accss data we post on as req.body
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-var lions = [];
-var id = 0;
+let lions = [{
+    'name': 'Simba',
+    'id': 1,
+    'age': 3,
+    'pride': 'the cool cats',
+    'gender': 'male'
+}];
+let id = 0;
 
-// TODO: make the REST routes to perform CRUD on lions
+app.get('/lions', (request, response) => {
+    response.send(JSON.stringify(lions));
+});
+
+app.get('/lions/:id', (request, response) => {
+    let lion = _.find(lions, (el) => {
+        return (el.id == request.params.id);
+    });
+    response.send(JSON.stringify(lion));
+});
+
+app.post('/lions', (request, response) => {
+    let fail = undefined;
+    let newLion = {
+        'name': '',
+        'id': 0,
+        'age': 0,
+        'pride': '',
+        'gender': ''
+    };
+    _.each(request.body, (val, key) => {
+        (newLion[key] != undefined) ? newLion[key] = val: fail = key;
+    });
+
+    if (!fail && request._body) {
+        lions.push(newLion);
+    } else {
+        res.status(404).send('The params: ' + fail + ' did not exist or errored');
+    }
+    response.send(JSON.stringify(newLion));
+});
+
+app.put('/lions/:id', (request, response) => {
+    let changedLion = _.findWhere(lions, { id: request.params.id });
+    if (changedLion) {
+        _.each(request.body, (val, key) => {
+            changedLion[key] = val;
+        });
+    } else {
+        res.status(404).send('Lion not found');
+    }
+    response.send(JSON.stringify(changedLion));
+})
+
+app.delete('/lions/:id', (request, response) => {
+    let lostLion = _.findWhere(lions, { id: request.params.id });
+    if (lostLion) {
+        lions = _.without(lions, lostLion);
+    } else {
+        res.status(404).send('Lion not found');
+    }
+    response.send(JSON.stringify(lostLion));
+})
 
 app.listen(3000);
 console.log('on port 3000');
