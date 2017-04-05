ee7ea5678e157dd93aae2cabd4ba44ec1d2023fa
Adrian Mui <adrian.mui@gmail.com>
2017-04-05 16:45:08 -0500

    blog schema to code schema

--- server/api/post/postModel.js
+++ server/api/post/postModel.js
@@ -1,8 +1,25 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+// has_many categories
 var PostSchema = new Schema({
-
+    title: {
+        type: String,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    categories: {
+        type: [],
+        required: true
+    }
 });
 
 module.exports = mongoose.model('post', PostSchema);
+"title": "What's new in Angular 4",
