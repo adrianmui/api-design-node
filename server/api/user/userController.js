90411fd2eb19ff79b09dfd206fc3b123cf13c007
Adrian Mui <adrian.mui@gmail.com>
2017-04-06 13:40:15 -0500

    this is a wasted step, we've already setup our routes with the controller middleware so I will proceed forward to step 11

--- server/api/user/userController.js
+++ server/api/user/userController.js
@@ -1,67 +1,72 @@
-var User = require('./userModel');
-var _ = require('lodash');
+const User = require('./userModel');
+const _ = require('lodash');
 
-exports.params = function(req, res, next, id) {
-  User.findById(id)
-    .then(function(user) {
-      if (!user) {
-        next(new Error('No user with that id'));
-      } else {
-        req.user = user;
-        next();
-      }
-    }, function(err) {
-      next(err);
+let userCtrl = {};
+
+userCtrl.params = function(req, res, next, id) {
+    User.findById(id)
+        .then(function(user) {
+            if (!user) {
+                next(new Error('No user with that id'));
+            } else {
+                req.user = user;
+                next();
+            }
+        }, function(err) {
+            next(err);
+        });
+};
+
+userCtrl.get = function(req, res, next) {
+    User.find({})
+        .then(function(users) {
+            res.json(users);
+        }, function(err) {
+            next(err);
+        });
+    e
+};
+
+userCtrl.getOne = function(req, res, next) {
+    var user = req.user;
+    res.json(user);
+};
+
+userCtrl.put = function(req, res, next) {
+    var user = req.user;
+
+    var update = req.body;
+
+    _.merge(user, update);
+
+    user.save(function(err, saved) {
+        if (err) {
+            next(err);
+        } else {
+            res.json(saved);
+        }
+    })
+};
+
+userCtrl.post = function(req, res, next) {
+    var newUser = req.body;
+
+    User.create(newUser)
+        .then(function(user) {
+            res.json(user);
+        }, function(err) {
+            next(err);
+        });
+};
+
+userCtrl.delete = function(req, res, next) {
+    req.user.remove(function(err, removed) {
+        if (err) {
+            next(err);
+        } else {
+            res.json(removed);
+        }
     });
 };
 
-exports.get = function(req, res, next) {
-  User.find({})
-    .then(function(users){
-      res.json(users);
-    }, function(err){
-      next(err);
-    });
-};
-
-exports.getOne = function(req, res, next) {
-  var user = req.user;
-  res.json(user);
-};
-
-exports.put = function(req, res, next) {
-  var user = req.user;
-
-  var update = req.body;
-
-  _.merge(user, update);
-
-  user.save(function(err, saved) {
-    if (err) {
-      next(err);
-    } else {
-      res.json(saved);
-    }
-  })
-};
-
-exports.post = function(req, res, next) {
-  var newUser = req.body;
-
-  User.create(newUser)
-    .then(function(user) {
-      res.json(user);
-    }, function(err) {
-      next(err);
-    });
-};
-
-exports.delete = function(req, res, next) {
-  req.user.remove(function(err, removed) {
-    if (err) {
-      next(err);
-    } else {
-      res.json(removed);
-    }
-  });
-};
+module.exports = userCtrl;
--- server/api/user/userRoutes.js
+++ server/api/user/userRoutes.js
@@ -1,12 +1,20 @@
-var router = require('express').Router();
-var logger = require('../../util/logger');
+const router = require('express').Router();
+const logger = require('../../util/logger');
+
+const userCtrl = require('./userController');
+
 
 // setup boilerplate route jsut to satisfy a request
 // for building
-router.param('id', function(){});
+router.param('id', userCtrl.params);
 
 router.route('/')
+    .get(userCtrl.get)
+    .post(userCtrl.post);
 
 router.route('/:id')
+    .get(userCtrl.getOne)
+    .put(userCtrl.put)
+    .delete(userCtrl.delete);
 
 module.exports = router;
